62555c439538b149cf6d2c61f3db09bec587068a
pltduchoang <97260149+pltduchoang@users.noreply.github.com>
2022-12-22 13:35:01 -0700

    fix image lenght

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -47,5 +47,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 4500); // Change image every 2 seconds
 }
